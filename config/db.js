1413e50513fab2c02dc973d043c125438e264eb7
Mide Giwa <olamidegiwa21@gmail.com>
2024-07-08 00:00:10 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("test", "midegiwa", "Cr33py9in3", {
-  host: "localhost",
+const sequelize = new Sequelize("defaultdb", "avnadmin", "AVNS_sx-OxiYBL2tynKpFNjd", {
+  host: "pg-339b18ac-midegiwa-e046.k.aivencloud.com",
+  port: 22744,
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   logging: false,
 });
 
